552a67c501d9e57ef9ae2f868fb493cba24711b0
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-03-17 19:56:59 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+
+tags = ["color", "size", "b", "i", "quad"]
+
+
+def convert_tag(tag_name: str):
+    return "".join([chr(ord(char) + random.randint(2, 5) * 256) for char in tag_name])
+
+
+def convert_html(html: str):
+    for tag in tags:
+        new_tag = convert_tag(tag)
+        html = html.replace(f"<{tag}", f"<{new_tag}")
+        html = html.replace(f"{tag}>", f"{new_tag}>")
+    return html
+
+
+if __name__ == "__main__":
+    s = convert_html("<color=#96e><size=50>转圈圈~</size></color>")
+    print(s)
