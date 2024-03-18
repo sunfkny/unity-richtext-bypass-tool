dfd046868a07cf1f435804aacff3f84f170e0d64
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-03-18 23:01:58 +0800

    py prop replace

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 
 tags = ["color", "size", "b", "i", "quad"]
+props = ["size"]
 
 
 def convert_tag(tag_name: str):
@@ -12,9 +13,21 @@ def convert_html(html: str):
         new_tag = convert_tag(tag)
         html = html.replace(f"<{tag}", f"<{new_tag}")
         html = html.replace(f"{tag}>", f"{new_tag}>")
+    for prop in props:
+        new_prop = convert_tag(prop)
+        html = html.replace(f"{prop}=", f"{new_prop}=")
     return html
 
 
 if __name__ == "__main__":
-    s = convert_html("<color=#96e><size=50>转圈圈~</size></color>")
+    s = convert_html("""
+<b>粗体</b>
+<i>斜体</i>
+<color=#96e>颜色</color>
+<size=50>大小</size>
+
+<color=#96e><size=50><i><b>转圈圈~</b></i></size></color>
+
+<quad size=50 />
+""")
     print(s)
